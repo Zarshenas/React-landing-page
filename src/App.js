9bfd223f9bb8db0047299c971aaadb956e276d34
Ali Zarshenas <ali.zarshenas.2018@gmail.com>
2022-09-13 23:54:57 +0430

    video section added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NavBar from './Components/NavBar.js';
 import Banner from './Components/Banner';
 import Test from './Components/test';
 import Features from './Components/Features';
+import Video from './Components/Video';
 
 class App extends Component {
   render() {
@@ -12,15 +13,7 @@ class App extends Component {
         <NavBar />
         <Banner />
         <Features />
-        <Test />
-        <Test />
-        <Test />
-        <Test />
-        <Test />
-        <Test />
-        <Test />
-        <Test />
-        <Test />
+        <Video />
         <Test />
         <Test />
         <Test />
---
+++ src/Components/Video.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import styles from './Video.module.css';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faPlay} from "@fortawesome/free-solid-svg-icons";
+import '../../node_modules/react-modal-video/scss/modal-video.scss';
+import ModalVideo from 'react-modal-video';
+
+class Video extends Component {
+        constructor(){
+            super();
+            this.state={
+                isOpen:false
+            }
+        }
+        openModal = () =>{
+            this.setState({
+                isOpen:true
+            })
+        }
+    render() {
+        return (
+                <section className={`${styles.videoSection} container flexcolumn`}>
+                    <ModalVideo channel='youtube' isOpen={this.state.isOpen} videoId='eBGIQ7ZuuiU' h1="dadadada" onClose={() => this.setState({isOpen: false})} />
+                <div className={styles.videoContainer}>
+                        <div className={styles.titlecon} >
+                            <div onClick={this.openModal} className={styles.playButtonCon}><FontAwesomeIcon fade={true} icon={faPlay} /></div>
+                            <h1 className={styles.title}>Watch Our <span>Tutorial</span> <br/> Videos.</h1>
+                        </div>
+                </div>
+                </section>
+        );
+    }
+}
+
+export default Video;
